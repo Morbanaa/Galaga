17ad330c371c1174e01ef759eee102bb41b9fbb2
Morbanaa <theodorerodd@gmail.com>
2026-05-19 16:11:56 -0500

    Enemy moves down screen. Enemy loosly tracks the player. If they leave the screen they spawn back at the top of it

--- enemy.py
+++ enemy.py
@@ -1,4 +1,24 @@
+# Teddy Rodd
+# Morbanaa Studios
+# Galaga
+
+import random
+
 class Enemy():
+    enemys = []
+    timer = 0
     def __init__(self,ypos,xpos):
         self.ypos = ypos
         self.xpos = xpos
+
+    def update(self,player,game_height,game_width):
+        self.ypos +=1
+        if abs(self.xpos -player.xpos) > random.randint(1,4):
+            if self.xpos > player.xpos:
+                self.xpos -=1
+            else:
+                self.xpos +=1
+        
+        if self.ypos > game_height + 10:
+            self.ypos = 0
+            self.xpos = random.randint(3,game_width -3)
