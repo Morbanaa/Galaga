5cb6213adf61f1cacf08dd8ef296169dc3b37e8c
Morbanaa <theodorerodd@gmail.com>
2026-05-19 13:32:28 -0500

    Added a consistent game speed

--- main.py
+++ main.py
@@ -2,14 +2,19 @@
 # Morbanaa Studios
 # Galaga
 
+import time
 from game_manager import Game_Manager
 
 def main():
-    game_manager = Game_Manager(25,80)
+    game_speed = .05
+    game_manager = Game_Manager(35,50)
     game_manager.world_gen() # creates world one time
 
     while True:
         game_manager.render_world()
+
+        # Util
+        time.sleep(game_speed)
         game_manager.clear_move_cursor()
 
 if __name__ == "__main__":
