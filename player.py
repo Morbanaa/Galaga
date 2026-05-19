1d84a4369ded856c6a938efaf6123bd5dd82039e
Morbanaa <theodorerodd@gmail.com>
2026-05-19 16:12:55 -0500

    Added player movement. They may not leave the game area. Also added shooting which consumes ammo

--- player.py
+++ player.py
@@ -1,4 +1,32 @@
+# Teddy Rodd
+# Morbanaa Studios
+# Galaga
+
+from bullet import Bullet
+import keyboard
+
 class Player():
-    def __init__(self,ypos,xpos):
+    def __init__(self,ypos,xpos,ammo):
         self.ypos = ypos
         self.xpos = xpos
+        self.ammo = ammo
+        self.timer = 7
+
+    def update(self,game_map):
+        if keyboard.is_pressed("W") and game_map[self.ypos -1][self.xpos] != "@":
+            self.ypos -= 1
+
+        if keyboard.is_pressed("S") and game_map[self.ypos +1][self.xpos] != "@":
+            self.ypos += 1
+
+        if keyboard.is_pressed("A") and game_map[self.ypos][self.xpos -1] != "@":
+            self.xpos -= 1
+
+        if keyboard.is_pressed("D") and game_map[self.ypos][self.xpos +1] != "@":
+            self.xpos += 1
+
+        self.timer += 1
+        if keyboard.is_pressed("SPACE") and self.ammo > 0 and self.timer >7:
+            self.timer = 0
+            Bullet.bullets.append(Bullet(self.ypos -1,self.xpos))
+            self.ammo -=1
