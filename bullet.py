b2ebfda0a6225b112f1e0104660a5b990be0a8a2
Morbanaa <theodorerodd@gmail.com>
2026-05-19 16:10:38 -0500

    Added bullet movement up the screen

--- bullet.py
+++ bullet.py
@@ -1,4 +1,12 @@
+# Teddy Rodd
+# Morbanaa Studios
+# Galaga
+
 class Bullet():
+    bullets = []
     def __init__(self,ypos,xpos):
         self.ypos = ypos
         self.xpos = xpos
+
+    def update(self):
+        self.ypos -=1
