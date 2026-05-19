6f33f66953f3c4be3f8e2ce7575a402c0ee473ae
Morbanaa <theodorerodd@gmail.com>
2026-05-19 11:39:04 -0500

    Set up main loop callling the methods to create and render the map

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-ECHO is on.
+# Teddy Rodd
+# Morbanaa Studios
+# Galaga
+
+from game_manager import Game_Manager
+
+def main():
+    game_manager = Game_Manager(25,80)
+    game_manager.world_gen() # creates world one time
+
+    while True:
+        game_manager.render_world()
+        game_manager.clear_move_cursor()
+
+if __name__ == "__main__":
+    main()
+
+
